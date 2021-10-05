fafa07c5c24c28de8723defe95a2ec12dee96ca8
CameoDaManeo <CameoDaManeo@gmail.com>
2021-10-05 17:17:47 +1300

    Initital commit

---
+++ TipsJSONConverter/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TipsJSONConverter
+{
+    public partial class Form1 : Form
+    {
+		//Constants and Class-Scope Variables
+		List<Tip> tipList = new List<Tip>(); //A list to store the tips to.
+		List<string> typeList = new List<string>(); //A list to store the types of tips to.
+
+		public Form1()
+        {
+            InitializeComponent();
+			
+			//Variables
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			StreamWriter logger;
+			StreamReader reader;
+			string streamFilePath, tipsFolderPath, individualTipPath;
+			string line;
+			string[] txtArray;
+			string type, text;
+			string[] formatArray;
+
+			//Set the filter and title for the open file dialog.
+			openFileDialog.Filter = "TXT Files|*.TXT|All Files|*.*";
+			openFileDialog.Title = "Open TXT file with tips on each line.";
+
+			//Check if the user has selected a stream file.
+			if (openFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				//Record the stream file path.
+				streamFilePath = openFileDialog.FileName;
+				
+				//Create the tips folder path, based on the stream file path and name.
+				tipsFolderPath = Path.GetDirectoryName(streamFilePath) + "\\" + Path.GetFileNameWithoutExtension(streamFilePath);
+				
+				//If the tips folder does not exist, create it.
+				if (!Directory.Exists(tipsFolderPath))
+				{
+					Directory.CreateDirectory(tipsFolderPath);
+				}
+				
+				//Open a log file in the tips folder.
+				logger = new StreamWriter(tipsFolderPath + "\\" + "log.txt");
+
+				//Write the path information to the log file.
+				logger.WriteLine("Tips generated from txt file at: \"" + streamFilePath + "\"");
+
+				//Open the selected file to stream.
+				reader = File.OpenText(streamFilePath);
+
+				//Repeat while it is not the end of the file.
+				while (!reader.EndOfStream)
+				{
+					try
+					{
+						//Read a line from the file.
+						line = reader.ReadLine();
+
+						//Split the line.
+						txtArray = line.Split('|');
+
+						//Check the size of the array.
+						if (txtArray.Length == 3)
+						{
+							//Get the data from txtArray.
+							type = txtArray[0].ToLower();
+							text = txtArray[1];
+							formatArray = txtArray[2].Split(',');
+
+							//If type is an empty string, change it to "tip".
+							if (type == "")
+							{
+								type = "my";
+							}
+							
+							//Create a new tip (if valid), add it to the tip list, and add any new tip types to the type list.
+							AddTip(type, text, formatArray);
+						}
+						else if(txtArray.Length == 1)
+                        {
+							//Get the data from txtArray.
+							type = "my";
+							text = txtArray[0];
+							formatArray = new string[] { "" };
+
+							//Create a new tip (if valid), add it to the tip list, and add any new tip types to the type list.
+							AddTip(type, text, formatArray);
+						}
+						else
+						{
+							logger.WriteLine("Line was not in the correct format: \"" + line + "\"");
+						}
+					}
+					catch (Exception ex)
+					{
+						logger.WriteLine(ex.Message);
+					}
+				}
+
+				//Close the stream file.
+				reader.Close();
+				
+				//For each type in the type list.
+				foreach (string t in typeList)
+				{
+					int j = 0;
+
+					individualTipPath = tipsFolderPath + "\\" + "assets" + "\\" + t + "\\" + "tips";
+
+					//Write the folder path of the current tips that are being written to the log file.
+					logger.WriteLine();
+					logger.WriteLine("\"" + individualTipPath + "\":");
+
+					//For each tip in the tip list.
+					foreach (Tip tip in tipList)
+					{
+						//If the type of the tip matches the current tip.
+						if (tip.Type == t)
+						{
+							//Write the tip to a JSON file.
+							tip.WriteJson(individualTipPath, j);
+
+							//Write the tip to the log file.
+							logger.WriteLine(tip.Text);
+
+							//Increase j by 1.
+							j++;
+						}
+					}
+				}
+
+				//Close the logger.
+				logger.Close();
+
+				//Open the directory in Windows Explorer.
+				Process.Start(tipsFolderPath);
+			}
+		}
+
+		/// <summary>
+		/// Creates a new tip (if valid), adds it to the tip list, and adds any new tip types to the type list.
+		/// </summary>
+		/// <param name="type"></param>
+		/// <param name="text"></param>
+		/// <param name="formatArray"></param>
+		private void AddTip(string type, string text, string[] formatArray)
+        {
+			//Check if text is not an empty string.
+			if (text != "")
+			{
+				//Create a new Tip and add it to the tip list.
+				Tip tip = new Tip(type, text, formatArray);
+				tipList.Add(tip);
+
+				//If the tip's type is not contained in the types list, then add it.
+				if (!typeList.Contains(type))
+				{
+					typeList.Add(type);
+				}
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+        {
+			Application.Exit();
+        }
+    }
+}
---
+++ TipsJSONConverter/Tip.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipsJSONConverter
+{
+    class Tip
+    {
+        //Fields
+        private string _type; //The type of the tip.
+        private string _text; //The text of the tip.
+        private string[] _formatArray; //The format of the tip.
+
+        /// <summary>
+        /// Constructor: Initialises the fields to the values passed in.
+        /// </summary>
+        /// <param name="type">The name space of the tip.</param>
+        /// <param name="text">The text to be displayed.</param>
+        /// <param name="formatArray">An array of tip formatting instructions in JSON format.</param>
+        public Tip(string type, string text, string[] formatArray)
+        {
+            _type = type;
+            _text = text;
+            
+            if (formatArray[0] != "")
+            {
+                _formatArray = formatArray;
+            }
+        }
+
+        /// <summary>
+        /// Gets the type of the tip.
+        /// </summary>
+        public string Type
+        {
+            get { return _type; }
+        }
+
+        /// <summary>
+        /// Gets the text of the tip.
+        /// </summary>
+        public string Text
+        {
+            get { return _text; }
+        }
+        
+        /// <summary>
+        /// Writes the tip to a JSON file.
+        /// </summary>
+        /// <param name="path">Directory to write the JSON to.</param>
+        /// <param name="i">Integer for file name.</param>
+        public void WriteJson(string path, int i)
+        {
+            //If the directory for the tip's type doesn't exist, create it.
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            //Create a new stream writer.
+            StreamWriter writer;
+
+            //Open a new file.
+            writer = new StreamWriter(path + "\\" + _type + "tip" + i + ".json");
+            
+            //Write the tip in JSON format.
+            writer.WriteLine("{");
+            writer.WriteLine("\t\"tip\":");
+            writer.WriteLine("\t{");
+            writer.Write("\t\t\"text\":");
+            writer.Write("\"" + _text + "\"");
+
+            //Check if _formatArray is not null.
+            if (_formatArray != null)
+            {
+                //For each format in _formatArray
+                foreach (string format in _formatArray)
+                {
+                    //Write the current format.
+                    writer.WriteLine(",");
+                    writer.Write("\t\t" + format);
+                }
+            }
+            
+            writer.WriteLine();
+            writer.WriteLine("\t}");
+            writer.WriteLine("}");
+
+            //Close the file.
+            writer.Close();
+        }
+    }
+}
